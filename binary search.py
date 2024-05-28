d60b58bbe93d85c1d3f7ddb376859a36758493c3
mogasalavarshi <164539187+mogasalavarshi@users.noreply.github.com>
2024-05-28 15:21:22 +0530

    Add files via upload

---
+++ binary search.py
@@ -0,0 +1,23 @@
+nums= [12,34,35,56,78]
+target = 35
+nums = sorted(nums)
+
+left = 0
+right = len(nums)-1
+flag = -1
+
+while left <= right:
+    mid = (left + right) // 2
+    if nums[mid] == target:
+        flag = mid
+        break
+    elif nums[mid] > target:
+        right = mid -1
+    else:
+        left = mid + 1
+
+if flag == -1:
+    print("Target not found")
+else:
+    print("Target found at index:", flag)
+    

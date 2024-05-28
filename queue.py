60b8d27164e6e399b419fa0cec553999a989257e
mogasalavarshi <164539187+mogasalavarshi@users.noreply.github.com>
2024-05-28 13:53:45 +0530

    Add files via upload

---
+++ queue.py
@@ -0,0 +1,22 @@
+def enqueue(Q, ele):
+    Q.append(ele)
+    print(ele, "inserted into queue successfullu")
+    
+def dequeue(Q):
+    if len(Q) == 0:
+        print("Queue is empty")
+        return
+    print(Q[0]," is getting deleted")
+    Q.pop(0)
+Q =[]
+
+enqueue(Q, 10)
+enqueue(Q, 20)
+enqueue(Q, 30)
+enqueue(Q, 40)
+
+dequeue(Q)
+dequeue(Q)
+dequeue(Q)
+dequeue(Q)
+          
